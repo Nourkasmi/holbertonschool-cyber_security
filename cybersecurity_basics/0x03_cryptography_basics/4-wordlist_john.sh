48e419311e3c12ada6ba28c12862a75b3c98adfa
Nourkasmi <nourkasmi451@gmail.com>
2025-11-09 16:17:38 +0100

    task

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-{ john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"; john --show "$1" | cut -d: -f2 | head -n -2 > 4-password.txt; }
+john --wordlist=rockyou.txt --format=raw-md5 "$1" 2>/dev/null; john --show --format=raw-md5 "$1" | cut -d: -f2 > 4-password.txt
