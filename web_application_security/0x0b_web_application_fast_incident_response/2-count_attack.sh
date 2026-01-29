e6bac3b7d41d70abc7dd519dfc8059f6168ed085
Nourkasmi <nourkasmi451@gmail.com>
2026-01-29 16:13:23 +0100

    letssee

--- web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-attacker=$(awk '{print $1}' $1 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
-awk -v ip=$attacker '$1 == ip {count++} END {print count}' $1
+awk '{print $1}' "logs.txt" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $1}'
