f4ec65317ea3d973a92c01ad76ac30bf487d1039
Nourkasmi <nourkasmi451@gmail.com>
2026-02-20 14:08:04 +0100

    again

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -5,11 +5,11 @@ def merge_json_files(file1_path, file2_path)
   data1 = JSON.parse(File.read(file1_path))
   data2 = JSON.parse(File.read(file2_path))
 
-  # Merge arrays (assuming both files contain JSON arrays)
   merged_data = data2 + data1
 
-  # Write back to file2
   File.open(file2_path, 'w') do |file|
     file.write(JSON.pretty_generate(merged_data))
   end
+
+  puts "Merged JSON written to #{file2_path}"
 end
